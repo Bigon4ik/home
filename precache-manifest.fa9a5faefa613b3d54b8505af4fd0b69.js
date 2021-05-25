bf193772ef743871059efd29a4192b500191dc38
Bigon4ik <bigon444ik@gmail.com>
2021-05-25 20:06:25 +0300

    Updates

---
+++ precache-manifest.fa9a5faefa613b3d54b8505af4fd0b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f02b6bb26af87ca0ca579f884f28298",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "af468354a061c902e420",
+    "url": "/simple-react-homeworks-master/static/css/main.d061e9ec.chunk.css"
+  },
+  {
+    "revision": "7502067bee41d89d0633",
+    "url": "/simple-react-homeworks-master/static/js/2.efd87063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-homeworks-master/static/js/2.efd87063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af468354a061c902e420",
+    "url": "/simple-react-homeworks-master/static/js/main.97dbd718.chunk.js"
+  },
+  {
+    "revision": "33f020c42a00208bc7a5",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.074d676d.js"
+  }
+]);
