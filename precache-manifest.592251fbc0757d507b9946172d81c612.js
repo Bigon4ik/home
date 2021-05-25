68700a735178b680e80302814f8d982025a598c8
Bigon4ik <bigon444ik@gmail.com>
2021-05-25 20:56:27 +0300

    Updates

---
+++ precache-manifest.592251fbc0757d507b9946172d81c612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99b35f772b76037ec9707d22f2e1740",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "4629a318ff49ac0b0488",
+    "url": "/simple-react-homeworks-master/static/css/main.d061e9ec.chunk.css"
+  },
+  {
+    "revision": "27521c49f67e520f2d65",
+    "url": "/simple-react-homeworks-master/static/js/2.00bb1637.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks-master/static/js/2.00bb1637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4629a318ff49ac0b0488",
+    "url": "/simple-react-homeworks-master/static/js/main.cc573ffb.chunk.js"
+  },
+  {
+    "revision": "33f020c42a00208bc7a5",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.074d676d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-react-homeworks-master//precache-manifest.72c06bee09d7b6c93b03bbd35e770888.js"
+  "/simple-react-homeworks-master/precache-manifest.592251fbc0757d507b9946172d81c612.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-react-homeworks-master//index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-react-homeworks-master/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
